7fa7bacca4d4569a90da02a299a757141c4e1009
Rafael Terra <rafaelstjf@hotmail.com>
2023-07-11 15:35:35 -0300

    adding classes

--- TP/alphafold.py
+++ TP/alphafold.py
@@ -1,4 +1,4 @@
-import requests, re, json, os, argparse, subprocess
+import requests, sys, json, os, argparse, subprocess
 
 """
 
@@ -13,45 +13,67 @@ import requests, re, json, os, argparse, subprocess
 Procesar una lista
 
 """
+class AlphaFolder:
 
-P2RANK_BIN = "/home/rafa_br/Documents/GitHub/p2rank_2.4/prank"
-FPOCKET_BIN = "/home/rafa_br/Documents/GitHub/fpocket/bin/fpocket"
-MAX_CPU = 4
-def GetAFFromUniprot(accession):
-    url = f'https://rest.uniprot.org/uniprotkb/{accession}.txt'
-    get_request = requests.get(url)
-    filename = f"{accession}_uniprot.pdb"
-    with open(filename, "wb") as f:
-        for chunk in get_request.iter_content(chunk_size=2**20):
-            f.write(chunk)
-    text = get_request.text
-    pattern = r"AlphaFoldDB;\s\S+;"
-    af = (re.search(pattern, text).group()).split(';')
-    return af[1].strip()
+    def __init__(self, accession, working_dir=os.getcwd(), p2rank_bin=None, fpocket_bin=None, max_cpu=1) -> None:
+        if p2rank_bin is not None:
+            self.P2RANK_BIN = p2rank_bin
+        else:    
+            self.P2RANK_BIN = "/home/rafa_br/Documents/GitHub/p2rank_2.4/prank"
+        if fpocket_bin is not None:
+            self.FPOCKET_BIN = fpocket_bin
+        else:
+            self.FPOCKET_BIN = f"docker run -v {working_dir}:{working_dir} -w {working_dir} --user {os.getuid()}:{os.getgid()} --rm -it ezequieljsosa/fpocket fpocket"
+        self.working_dir = working_dir
+        self.accession = accession
+        self.uniprot_url = f'https://rest.uniprot.org/uniprotkb/{accession}.txt'
+        self.af_url = f"https://alphafold.ebi.ac.uk/api/prediction/{accession}?key=AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94"
+        self.result_dir = os.path.join(working_dir, "output")
+        self.MAX_CPU = max_cpu
+        self.uniprot_text_filename = os.path.join(self.working_dir, f"{accession}_uniprot.txt")
+        self.af_pdb_filename = os.path.join(self.working_dir,f"{accession}_AF.pdb")
+        sys.stderr.write(f"Uniprot protein accession number: {self.accession}\n" +
+                         f"P2RANK binaries: {self.P2RANK_BIN}\n" +
+                         f"FPOCKET binaries: {self.FPOCKET_BIN}\n" + 
+                         f"Working dir: {self.working_dir}\n" +
+                         f"Maximum cores: {self.MAX_CPU}\n"
+                         f"Output dir: {self.result_dir}\n"
+                         )
+        pass
 
-def GetAlphaFoldPrediction(accession):
-    url = f"https://alphafold.ebi.ac.uk/api/prediction/{accession}?key=AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94"
-    metadata = json.loads(requests.get(url).text)
-    pdb_text = requests.get(metadata[0]["pdbUrl"]).text
-    with open(f"{accession}_AF.pdb", "w") as f:
-        f.write(pdb_text)
-
+    def GetUniprotFile(self):
+        get_request = requests.get(self.uniprot_url)
+        with open(self.uniprot_text_filename, "wb") as f:
+            for chunk in get_request.iter_content(chunk_size=2**20):
+                f.write(chunk)
+    
+    def GetAlphaFoldPrediction(self):
+        metadata = json.loads(requests.get(self.af_url).text)
+        pdb_text = requests.get(metadata[0]["pdbUrl"]).text
+        with open(self.af_pdb_filename, "w") as f:
+            f.write(pdb_text)
+    
+    def RunP2rankFromFile(self):
+        sts = subprocess.Popen(f"{self.P2RANK_BIN} predict" +  f" -o {os.path.join(self.result_dir, f"{self.accession} + ")} -visualizations 0 -threads {self.MAX_CPU} -c alphafold -f {self.accession}_AF.pdb", shell=True, stdout=sys.stderr).wait()
+    
+    def RunFpocketFromFile(self):
+        sts = subprocess.run(self.FPOCKET_BIN + f" -f {self.af_pdb_filename}", shell=True, stdout=sys.stderr)
+        fpocket_out = f"{self.accession}_AF_out"
+        path_exists = os.path.exists(os.path.join(self.working_dir, fpocket_out))
+        if path_exists:
+            if not os.path.exists(self.result_dir):
+                os.mkdir(self.result_dir)
+            os.system(f"mv {os.path.join(self.working_dir, fpocket_out)} {os.path.join(self.result_dir, fpocket_out)}")
+        
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help="protein's PDB accession number", type=str, required=True)
-    parser.add_argument('-o', '--output', help="path of the output", type=str, required=False, default=os.getcwd())
+    parser.add_argument('-o', '--working_dir', help="path of the working_dir", type=str, required=False, default=os.getcwd())
     args = parser.parse_args()
-    working_dir = args.output
-    sys.stderr.write(f"Uniprot protein accession number: {args.input}\n")
-    accession = GetAFFromUniprot(args.input)
-    GetAlphaFoldPrediction(accession)
-    print(f"Uniprot Alpha Fold DB entry: {accession}")
-    out_dir = os.path.join(working_dir, "output")
-    sts = subprocess.Popen(f"{P2RANK_BIN} predict" +  f" -o {os.path.join(out_dir, accession)} -visualizations 0 -threads {MAX_CPU} -c alphafold -f {accession}_AF.pdb", shell=True).wait()
-    # sts = subprocess.Popen(f"{FPOCKET_BIN}" +  f" -f {accession}_AF.pdb", shell=True).wait()
-    sts = subprocess.run(f"docker run -v {working_dir}:{working_dir} -w {working_dir} --user {os.getuid()}:{os.getgid()} --rm -it ezequieljsosa/fpocket fpocket -f {os.path.join(working_dir, accession)}_AF.pdb", shell=True)
-    fpocket_out = f"{accession}_AF_out"
-    path_exists = os.path.exists(os.path.join(working_dir, fpocket_out))
-    if path_exists:
-        os.system(f"mv {os.path.join(working_dir, fpocket_out)} {os.path.join(out_dir, fpocket_out)}")
+    working_dir = args.working_dir
+    obj = AlphaFolder(args.input)
+    obj.GetAlphaFoldPrediction()
+    obj.RunP2rankFromFile()
+    obj.RunFpocketFromFile()
