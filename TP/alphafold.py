b3b2d74c9937e4896752bca8f00ac073aacada61
Rafael Terra <rafaelstjf@hotmail.com>
2023-07-13 12:01:57 -0300

    functions to get the PLDDT and compress files

--- TP/alphafold.py
+++ TP/alphafold.py
@@ -1,20 +1,7 @@
-import requests, sys, json, os, argparse, subprocess, tqdm
+import requests, sys, json, os, argparse, subprocess, tqdm, tarfile, shutil
 import concurrent.futures
 import Bio.PDB as biopdb
 
-"""
-
-- FPOCKET_BIN with docker line
-- Pasar a Clase
-- P2RANK_BIN, FPOCKET_BIN, MAX_CPU default o que se puedan poner en el contructor
-- template de la url, tambien variables de la clase
-- usar para contro sys.stderr
-- pLDDT descargar (scrore por residuo)
-- .gz
-
-Procesar una lista
-
-"""
 class AlphaFolder:
 
     def __init__(self, accession, working_dir=os.getcwd(), p2rank_bin=None, fpocket_bin=None, max_cpu=1) -> None:
@@ -30,10 +17,14 @@ class AlphaFolder:
         self.accession = accession
         self.uniprot_url = f'https://rest.uniprot.org/uniprotkb/{accession}.txt'
         self.af_url = f"https://alphafold.ebi.ac.uk/api/prediction/{accession}?key=AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94"
-        self.result_dir = os.path.join(working_dir, "output")
+        self.result_dir = os.path.join(working_dir, os.path.join("output", self.accession))
+        if not os.path.exists(self.result_dir):
+                os.mkdir(self.result_dir)
+        self.out_dir = os.path.join(self.working_dir, "output")
         self.MAX_CPU = max_cpu
         self.uniprot_text_filename = os.path.join(self.result_dir, f"{accession}_uniprot.txt")
         self.af_pdb_filename = os.path.join(self.result_dir,f"{accession}_AF.pdb")
+        self.af_plddt_filename = os.path.join(self.result_dir,f"{accession}_pldd.json")
         sys.stderr.write(f"Uniprot protein accession number: {self.accession}\n" +
                          f"P2RANK binaries: {self.P2RANK_BIN}\n" +
                          f"FPOCKET binaries: {self.FPOCKET_BIN}\n" + 
@@ -44,43 +35,76 @@ class AlphaFolder:
         pass
 
     def GetUniprotFile(self):
+        """Get the text file containing the features of a protein from Uniprot. The file is stored at output/{accesion}
+        """
         get_request = requests.get(self.uniprot_url)
         with open(self.uniprot_text_filename, "wb") as f:
             for chunk in get_request.iter_content(chunk_size=2**20):
                 f.write(chunk)
     
     def GetAlphaFoldPrediction(self):
+        """Get the PDB file containing the AlphaFold prediction from its DB. The file is stored at output/{accesion}
+        """
         metadata = json.loads(requests.get(self.af_url).text)
         pdb_text = requests.get(metadata[0]["pdbUrl"]).text
         with open(self.af_pdb_filename, "w") as f:
             f.write(pdb_text)
     
     def RunP2rankFromFile(self):
+        """Run P2RANK app from the PDB file obtained from AlphaFold DB and saves the results on output/{accesion} folder
+        """
         sts = subprocess.Popen(f"{self.P2RANK_BIN} predict" +  f" -o {os.path.join(self.result_dir, self.accession + '_p2rank')} -visualizations 0 -threads {self.MAX_CPU} -c alphafold -f {self.af_pdb_filename}", shell=True, stdout=sys.stderr).wait()
     
     def RunFpocketFromFile(self):
+        """Run FPOCKET app from the PDB file obtained from AlphaFold DB and saves the results on output/{accesion} folder
+        """
         sts = subprocess.run(self.FPOCKET_BIN + f" -f {self.af_pdb_filename}", shell=True, stdout=sys.stderr)
         fpocket_out = f"{self.accession}_AF_out"
         path_exists = os.path.exists(os.path.join(self.working_dir, fpocket_out))
         if path_exists:
-            if not os.path.exists(self.result_dir):
-                os.mkdir(self.result_dir)
             os.system(f"mv {os.path.join(self.working_dir, fpocket_out)} {os.path.join(self.result_dir, fpocket_out)}")
         
-    def GetPlddtFromFile(self):
+    def GetPlddtFromFile(self, save_to_file = True):
+        """Get the PLDDT values from all the residues in the PDB file from AlphaFold's DB
+
+        Args:
+            save_to_file (bool, optional): Defaults to True.
+
+        Returns:
+            Dictionary containing all the pair residue, value
+        """
         plddt = dict()
         parser = biopdb.PDBParser()
         structure = parser.get_structure(self.accession, self.af_pdb_filename)
-        for atom in structure.get_atoms():
-            plddt[atom.get_serial_number()] = atom.get_bfactor()
+        for r in structure.get_residues():
+            atoms = list(r.get_atoms())
+            plddt[r.get_resname()] = atoms[0].get_bfactor()
+        if save_to_file:
+            with open(self.af_plddt_filename, 'w') as jfile:
+                json.dump(plddt, jfile)
         return plddt
     
+    def CompressResults(self, remove_files = True):
+        """Compress all the results present in the protein's accession number folder
+
+        Args:
+            remove_files (bool, optional): Defaults to True.
+        """
+        tar_filename = os.path.join(self.out_dir, f"{self.accession}.tar.gz")
+        with tarfile.open(tar_filename, mode='w:gz') as archive:
+            archive.add(self.result_dir, self.accession, recursive=True)
+        if remove_files:
+            shutil.rmtree(self.result_dir)
+
+
 if __name__ == "__main__":
     accessions = list()
     parser = argparse.ArgumentParser()
     if not sys.stdin.isatty():
         # there is something in the stdin
-        parser.add_argument('stdinput', nargs='?', type=argparse.FileType('r'), default=(None if sys.stdin.isatty() else sys.stdin))
+        parser.add_argument('stdinput', nargs='?', type=argparse.FileType('r'),
+                             default=(None if sys.stdin.isatty() else sys.stdin),
+                             help="List of protein's PDB accession numbers separated with new lines")
     else:
         parser.add_argument('-i', '--input', help="protein's PDB accession number", type=str, required=True)
     parser.add_argument('-o', '--working_dir', help="path of the working_dir", type=str, required=False, default=os.getcwd())
@@ -96,4 +120,5 @@ if __name__ == "__main__":
         obj.GetAlphaFoldPrediction()
         obj.RunP2rankFromFile()
         obj.RunFpocketFromFile()
-        #obj.GetPlddtFromFile()
+        obj.GetPlddtFromFile()
+        obj.CompressResults()
