8594d31771b653678fa6fdc4fb32e3dd0a665fb5
Rafael Terra <rafaelstjf@hotmail.com>
2023-07-13 16:40:55 -0300

    adapting it to class

--- TP/intersect.py
+++ TP/intersect.py
@@ -6,13 +6,13 @@ import numpy as np
 
 class Intersector:
     def __init__(self, accession) -> None:
-        self.p2rank_file = ""
+        self.p2rank_file = "/home/rafa_br/Documents/GitHub/targetpathogen/TP/output/P9WPB2/P9WPB2_p2rank/P9WPB2_AF.pdb_predictions.csv"
         self.p2_df = pd.read_csv(self.p2rank_file, 
                                    sep='\s*,\s*',
                                    usecols=["name", "residue_ids"],
                                    skipinitialspace=True, engine='python')
-        self.interpro_scan_file = ""
-        self.interpro_df = pd.read_csv(interpro_scan_file,
+        self.interpro_scan_file = "~/Downloads/iprscan5-R20230713-163003-0060-38200671-p1m.tsv"
+        self.interpro_df = pd.read_csv(self.interpro_scan_file,
                                     sep='\t', 
                                     header=None, 
                                     usecols=[3, 4, 6, 7], 
@@ -21,7 +21,7 @@ class Intersector:
         
     def CalcIntersections(self):
         colnames = ["Family", "Identifier"]
-        for i in range(0, df.shape[1]):
+        for i in range(0, self.p2_df.shape[0]):
             colnames.append(f"Points of intersection with Pocket {i+1}")
         self.df = pd.DataFrame(columns=colnames)
         for ind, row in self.interpro_df.iterrows():
@@ -32,34 +32,21 @@ class Intersector:
             intersections = dict()
             for j, p2row in self.p2_df.iterrows():
                 adjacent_residues = list(p2row["residue_ids"].split(' '))
-                adjacent_residues = np.asarray(int(x[2:]) for x in adjacent_residues)
-                intersections[p2row["name"]] = np.where(adjacent_residues >= range_inf and adjacent_residues <= range_sup, adjacent_residues)
+                adjacent_residues = list(int(x[2:]) for x in adjacent_residues)
+                intersections[p2row["name"]] = list()
                 for res in adjacent_residues:
-                    #if res >= range_inf and res <= range_sup:
-                    #    intersections[p2row["name"]].append(str(res))
-            values = [cur_name, cur_identifier]
+                    if res >= range_inf and res <= range_sup:
+                        intersections[p2row["name"]].append(str(res))
+            values = [cur_fam, cur_identifier]
             for k in intersections.keys():
                 values.append((', ').join(intersections[k]))
-            df.loc[len(df.index)] = values
+            self.df.loc[len(self.df.index)] = values
 ###################################################
-p2rank_file = "/home/rafa_br/Documents/GitHub/targetpathogen/TP/output/P9WPB2/P9WPB2_p2rank/P9WPB2_AF.pdb_predictions.csv"
-
-p2rank_predictions = pd.read_csv(p2rank_file, sep='\s*,\s*', usecols=["name", "residue_ids"], skipinitialspace=True, engine='python')
-
-print("P2Rank predictions")
-print(p2rank_predictions)
-
-
-interpro_scan_file = "~/Downloads/iprscan5-R20230713-163003-0060-38200671-p1m.tsv"
-interpro_scan = pd.read_csv(interpro_scan_file, sep='\t', header=None, usecols=[3, 4, 6, 7], names=["name", "identifier", "range_inf", "range_sup"])
-print("InterproScan")
-print(interpro_scan)
-lnames = ["Family", "Identifier"]
-lnames.extend("Points of intersection on " + p2rank_predictions["name"])
-df = pd.DataFrame(columns=lnames)
 
 
 
-print("-----------------------------------")
-print(df)
-df.to_csv("teste.csv", sep=',', index=False)
+
+if __name__ == "__main__":
+    obj = Intersector("a")
+    obj.CalcIntersections()
+    obj.df.to_csv("teste.csv", sep=',', index=False)
